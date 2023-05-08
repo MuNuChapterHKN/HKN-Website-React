999bd0b71aa6103fbc102299aceffb25a5f24414
Francesco <fbaldini02@gmail.com>
2023-05-08 21:57:10 +0200

    Button Border fixed

--- components/molecules/RoundButton.tsx
+++ components/molecules/RoundButton.tsx
@@ -3,7 +3,7 @@ import styles from "@/styles/components/molecules/RoundButton.module.css";
 const RoundButton = ({ onClick, text, children, className, style }: { onClick: () => void, text: string, children: any, className: any, style: any}) => {
 
     return (
-        <button style={style} className={`${styles.buttonMine} ${className}`} onClick={onClick}>
+        <button style={style} className={`${styles.button} ${className}`} onClick={onClick}>
             <text className={styles.text}>{text}</text>
             {children}
         </button>
