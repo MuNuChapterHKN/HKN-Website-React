afac61d02107fd766f6fbca3c95481988c7db0d3
Alberto Baroso <albertobaroso999@gmail.com>
2023-10-03 22:08:23 +0200

    feat: Homepage text and images

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,11 +32,15 @@ export default function Home() {
                     <text className={styles.drivenByPassion}>Driven By Passion</text>
                     <text className={styles.drivenByPassion}>Guided By Values</text>
                     <text className={styles.welcomeTo}>Welcome to our world, we are the Mu Nu Chapter of IEEE-HKN at
-                        the Polytechnic University of Turin.
+                                                       the Polytechnic University of Turin.
                     </text>
                     <div className={styles.titleButtons}>
-                        <RoundButton className={styles.joinUsTitleButton} onClick={() => router.push('/JoinUs')}>JOIN US</RoundButton>
-                        <RoundButton className={styles.aboutUsTitleButton} onClick={() => router.push('/AboutUs')}>ABOUT US</RoundButton>
+                        <RoundButton className={styles.joinUsTitleButton} onClick={() => router.push('/JoinUs')}>
+                            JOIN US
+                        </RoundButton>
+                        <RoundButton className={styles.aboutUsTitleButton} onClick={() => router.push('/AboutUs')}>
+                            ABOUT US
+                        </RoundButton>
                     </div>
                 </div>
                 {(windowWidth == 0 || windowWidth > 1100) &&
@@ -48,14 +52,14 @@ export default function Home() {
 
             <div className={styles.awardsContainer}>
                 <div className={styles.awards}>
-                    <Image className={styles.outstandingAward} src="/Home/outstanding-2019.png" alt="Award" width={228}
-                           height={200}/>
-                    <Image className={styles.outstandingAward} src="/Home/outstanding-2020.png" alt="Award" width={228}
-                           height={200}/>
-                    <Image className={styles.outstandingAward} src="/Home/outstanding-2021.png" alt="Award" width={228}
-                           height={200}/>
-                    <Image className={styles.outstandingAward} src="/Home/outstanding-2022.png" alt="Award" width={228}
-                           height={200}/>
+                    <Image className={styles.outstandingAward} src="/Home/outstanding-2019.png"
+                           alt="Outstanding Chapter Award 2019" width={228} height={200}/>
+                    <Image className={styles.outstandingAward} src="/Home/outstanding-2020.png"
+                           alt="Outstanding Chapter Award 2020" width={228} height={200}/>
+                    <Image className={styles.outstandingAward} src="/Home/outstanding-2021.png"
+                           alt="Outstanding Chapter Award 2021" width={228} height={200}/>
+                    <Image className={styles.outstandingAward} src="/Home/outstanding-2022.png"
+                           alt="Outstanding Chapter Award 2022" width={228} height={200}/>
                 </div>
             </div>
 
@@ -63,15 +67,16 @@ export default function Home() {
                 <div className={styles.visionLeft}>
                     <text className={styles.visionText}>VISION</text>
                     <text className={styles.visionTitle}>Discover Mu Nu Chapter</text>
-                    <text className={styles.vision}>Since 2017 we are dedicated to encouraging excellence in the
-                        IEEE-designated fields of interest, continuing to reinvent ourselves to meet the needs of our
-                        members and society overall
+                    <text className={styles.vision}>Since 2017 we are dedicated to the pursuit of a complementary path
+                                                    to our academic studies which encourages the excellence in the
+                                                    IEEE-designated fields of interest.
                     </text>
                     <div className={styles.visionButtons}>
                         <RoundButton className={styles.peopleVisionButton}
                                      onClick={() => router.push('/People')}>READ ME</RoundButton>
                         {/*TODO: add link*/}
-                        <RoundButton className={styles.galleryVisionButton} onClick={() => router.push('/404')}>PHOTO GALLERY</RoundButton>
+                        <RoundButton className={styles.galleryVisionButton} onClick={() => router.push('/404')}>PHOTO
+                                                                                                                GALLERY</RoundButton>
                     </div>
                 </div>
                 <img className={styles.visionImage} src="/Home/vision.jpg" alt="Vision"/>
@@ -84,12 +89,12 @@ export default function Home() {
                     <text className={styles.originsText}>ORIGINS</text>
                     <text className={styles.originsTitle}>Eta Kappa Nu, IEEE Honor Society</text>
                     <text className={styles.origins}>Founded in 1904, Eta Kappa Nu (IEEE-HKN), the honor society of
-                        IEEE, promotes the ideals of Scholarship, Character and Attitude. Catch a glimpse to our
-                        origins.
+                                                     IEEE, promotes the ideals of Scholarship, Character, and Attitude.
+                                                     Catch a glimpse of our origins.
                     </text>
                     <div className={styles.originsButtons}>
                         <RoundButton className={styles.readMeOriginsButton} onClick={() => router.push('/404')}>READ
-                            ME</RoundButton>
+                                                                                                                ME</RoundButton>
                         {/*TODO: add link*/}
                     </div>
                 </div>
@@ -107,9 +112,12 @@ export default function Home() {
                         </div>
                         <text className={styles.projectTitle}>Sirius: Vedere il futuro con gli occhi del presente</text>
                         <text className={styles.project}>Per la prima volta sono i ragazzi di HKN a far visita ad
-                            un’azienda: la SIRIUS (Sirius | Home). Per i nuovi associati questa è stata la prima
-                            esperienza di formazione interna, ma la vera novità ha coinvolto i già associati: potersi
-                            recare in prima persona a visitare un’impresa si è rivelata un’esperienza ...
+                                                         un’azienda: la SIRIUS (Sirius | Home). Per i nuovi associati
+                                                         questa è stata la prima
+                                                         esperienza di formazione interna, ma la vera novità ha
+                                                         coinvolto i già associati: potersi
+                                                         recare in prima persona a visitare un’impresa si è rivelata
+                                                         un’esperienza ...
                         </text>
                         {/*TODO: add link*/}
                     </div>
@@ -121,9 +129,11 @@ export default function Home() {
                         <text className={styles.projectTitle}>Vivere l’associazione Online: una guida per principianti
                         </text>
                         <text className={styles.project}>Inizialmente l’idea è nata dalla solitudine. Starsene chiusi in
-                            casa e non vedere nessuno per via del lockdown era una condizione che se all’inizio poteva
-                            sembrare esaltante e quasi eroica, con l’andar del tempo era diventata devastante. Il
-                            pensiero di entrare a far parte di una honor society ...
+                                                         casa e non vedere nessuno per via del lockdown era una
+                                                         condizione che se all’inizio poteva
+                                                         sembrare esaltante e quasi eroica, con l’andar del tempo era
+                                                         diventata devastante. Il
+                                                         pensiero di entrare a far parte di una honor society ...
                         </text>
                         {/*TODO: add link*/}
                     </div>
@@ -133,9 +143,11 @@ export default function Home() {
                         </div>
                         <text className={styles.projectTitle}>POLITO, HKN e Covid-19: una riflessione</text>
                         <text className={styles.project}>Ore diciassette di una silenziosa domenica di marzo. Silenziosa
-                            perché le strade sono deserte e più forte che mai risuonano soltanto i nostri pensieri. In
-                            questi casi non resta che dar loro una forma proiettandoli in mondi molto più vasti rispetto
-                            alle mura in cui siamo confinati in questi ...
+                                                         perché le strade sono deserte e più forte che mai risuonano
+                                                         soltanto i nostri pensieri. In
+                                                         questi casi non resta che dar loro una forma proiettandoli in
+                                                         mondi molto più vasti rispetto
+                                                         alle mura in cui siamo confinati in questi ...
                         </text>
                         {/*TODO: add link*/}
                     </div>
@@ -147,7 +159,7 @@ export default function Home() {
                 <text className={styles.textJoinOurChapter}>Join Our Chapter &</text>
                 <text className={styles.textJoinOurChapter}>Become The Next HKNuer</text>
                 <RoundButton className={styles.buttonJoinUs} onClick={() => router.push('/JoinUs')}>JOIN
-                    US</RoundButton>
+                                                                                                    US</RoundButton>
             </div>
 
         </Layout>
