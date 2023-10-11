57ae277cbbcb651b87650c3571a3d225aba88ebe
Alberto Baroso <albertobaroso999@gmail.com>
2023-10-11 14:22:51 +0200

    fix: improved performances by compressing images

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import '../styles/fonts.css'
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <link rel="shortcut icon" href="/images/favicon.ico" />
+    </Head>
+    <Component {...pageProps} />
+    </>
 }
