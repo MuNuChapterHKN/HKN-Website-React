c58c674596996a219e890b36eaca17a734f7179c
eltithecoder <gjiritielti@gmail.com>
2024-02-12 17:11:41 +0100

    fixed left arrow in publications not working

--- pages/Publications/Recognitions.tsx
+++ pages/Publications/Recognitions.tsx
@@ -47,7 +47,7 @@ export default function JoinUs() {
         if (awardIndex == 0) {
             setAwardIndex(awards.length - 1);
         } else {
-            setAwardIndex(mentionIndex - 1);
+            setAwardIndex(awardIndex - 1);
         }
     }
 
