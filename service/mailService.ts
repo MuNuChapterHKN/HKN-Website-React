46de90d7bb531ece622fc2ac5ec2e7aa5bcd7809
Alberto Baroso <albertobaroso999@gmail.com>
2024-02-24 15:14:19 +0100

    fix: added 'subject' and 'to' fields to sendEmail

--- service/mailService.ts
+++ service/mailService.ts
@@ -1,35 +1,39 @@
 import nodemailer from "nodemailer";
 
-export async function sendEmail(name: string, text: string, attachments: { filename: string; path: string; }[]): Promise<void> {
+export async function sendEmail(
+  subject: string,
+  body: string,
+  to: string = "recruitment@hknpolito.org",
+  attachments: { filename: string; path: string }[] = []
+): Promise<void> {
+  const transporter = nodemailer.createTransport({
+    // @ts-ignore
+    service: "gmail",
+    auth: {
+      type: process.env.AUTH_TYPE,
+      user: process.env.USER,
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      refreshToken: process.env.REFRESH_TOKEN,
+      accessToken: process.env.ACCESS_TOKEN,
+    },
+  });
 
-    const transporter = nodemailer.createTransport({
-        // @ts-ignore
-        service: "gmail",
-        auth: {
-            type: process.env.AUTH_TYPE,
-            user: process.env.USER,
-            clientId: process.env.CLIENT_ID,
-            clientSecret: process.env.CLIENT_SECRET,
-            refreshToken: process.env.REFRESH_TOKEN,
-            accessToken: process.env.ACCESS_TOKEN,
-        },
-    })
+  const mailOptions = {
+    from: process.env.HKN_RECRUITMENT_EMAIL,
+    to,
+    subject,
+    attachments,
+    body,
+  };
 
-    const mailOptions = {
-        from: 'recruitment@hknpolito.org',
-        to: 'recruitment@hknpolito.org',
-        subject: `RECRUITMENT - ${name} SUBMISSION`,
-        attachments,
-        text,
-    };
-
-    await new Promise<void>((resolve, reject) => {
-        transporter.sendMail(mailOptions, (err: any, response: any) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(response);
-            }
-        });
+  await new Promise<void>((resolve, reject) => {
+    transporter.sendMail(mailOptions, (err: any, response: any) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(response);
+      }
     });
+  });
 }
