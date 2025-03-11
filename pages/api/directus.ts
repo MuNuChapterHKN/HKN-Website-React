975d0ce1dbf70442df1efeeb192a160b8d225a39
Alessio <s294856@studenti.polito.it>
2025-03-11 14:33:28 +0100

    Study group from directus

--- pages/api/directus.ts
+++ pages/api/directus.ts
@@ -1,51 +1,65 @@
-import { createDirectus, rest, readAssetArrayBuffer, readItem, readFile, readProviders, staticToken } from '@directus/sdk';
+import StudyGroup, {StudyGroupProps} from "@/components/Events/StudyGroup";
+import { createDirectus, rest, readAssetArrayBuffer, readItems, readFile, readProviders, staticToken } from '@directus/sdk';
 
 
-const getMemberImage = async (teamId: number = 1) => {
-	const client = createDirectus('https://hknpolito.org/directus/').with(rest());
-	//console.log(client);
+// const getMemberImage = async (teamId: number = 1) => {
+// 	const client = createDirectus('https://hknpolito.org/directus/').with(rest());
+// 	//console.log(client);
 	
-	const told = await client.request(readItem('team', teamId, {
-		fields: ['*.*']
-	}));
-	const stream = await client.request(readAssetArrayBuffer(told.image.id));
+// 	const told = await client.request(readItem('team', teamId, {
+// 		fields: ['*.*']
+// 	}));
+// 	const stream = await client.request(readAssetArrayBuffer(told.image.id));
 
-    return new ImageData(stream, told.image.width, told.image.height);
+//     return new ImageData(stream, told.image.width, told.image.height);
 
-	//try{
-	//	const result = await client.request(readItems('team', {
-	//		fields: ['*.*'],
-	//		//deep: {
-	//		//	members: {
-	//		//		_filter: {
-	//		//			alumno: {
-	//		//				_eq: false
-	//		//			}
-	//		//		}
-	//		//	}
-	//		//}
-	//	}));
-	//	//console.log(result);
-	//	for (it of result) {
-	//		console.log(it);
-	//		it.members.forEach(async (it2)=> {
-	//			console.log(it2)
-	//			const stream = await client.request(readAssetArrayBuffer(it2.image));
-	//			console.log(stream);
-	//			//const img = Uint8Array.from(stream);
-	//			//console.log(img);
+// 	//try{
+// 	//	const result = await client.request(readItems('team', {
+// 	//		fields: ['*.*'],
+// 	//		//deep: {
+// 	//		//	members: {
+// 	//		//		_filter: {
+// 	//		//			alumno: {
+// 	//		//				_eq: false
+// 	//		//			}
+// 	//		//		}
+// 	//		//	}
+// 	//		//}
+// 	//	}));
+// 	//	//console.log(result);
+// 	//	for (it of result) {
+// 	//		console.log(it);
+// 	//		it.members.forEach(async (it2)=> {
+// 	//			console.log(it2)
+// 	//			const stream = await client.request(readAssetArrayBuffer(it2.image));
+// 	//			console.log(stream);
+// 	//			//const img = Uint8Array.from(stream);
+// 	//			//console.log(img);
 
-	//		});
-	//	}
-	//} catch(e) {
-	//	console.log('Got exeptions at readItem')
-	//	console.log(e);
-	//}
-	//try{
-	//	const result = await client.request(readFile('bc90e3dd-ee54-453b-a602-61959dc63e8e'));
-	//	console.log(result);
-	//} catch(e) {
-	//	console.log('Got exeptions at readFile')
-	//	console.log(e);
-	//}
+// 	//		});
+// 	//	}
+// 	//} catch(e) {
+// 	//	console.log('Got exeptions at readItem')
+// 	//	console.log(e);
+// 	//}
+// 	//try{
+// 	//	const result = await client.request(readFile('bc90e3dd-ee54-453b-a602-61959dc63e8e'));
+// 	//	console.log(result);
+// 	//} catch(e) {
+// 	//	console.log('Got exeptions at readFile')
+// 	//	console.log(e);
+// 	//}
+// }
+
+
+
+export async function fetchActiveStudyGroups() {
+	const directus = createDirectus('https://hknpolito.org/directus/').with(rest());
+	const studyGroups = await directus.request(
+		readItems('study_group', {
+			"filter": { "active": { _eq: true } },
+		})
+	);
+	return studyGroups as StudyGroupProps[];
 }
+
